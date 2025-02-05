9925106982fd099deb1e0d91c59ac5bc14655b7e
Oliseh Genesis <studyug@hotmail.com>
2025-02-05 10:04:16 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -48,7 +48,7 @@ app.post('/api/analyze', async (req, res) => {
 });
 
 // Start server
-app.listen(3000, '0.0.0.0', () => {
+app.listen(3450, '0.0.0.0', () => {
   console.log('Server is running on http://0.0.0.0:3000');
 });
 
